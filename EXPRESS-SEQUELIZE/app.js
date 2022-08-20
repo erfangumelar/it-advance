bfbf8da6917de6bda0aadca778fdca95d2a3a727
Erfan Gumelar <dupanks@gmail.com>
2022-08-20 23:16:20 +0700

    finish express.js part 5

--- EXPRESS-SEQUELIZE/app.js
+++ EXPRESS-SEQUELIZE/app.js
@@ -4,7 +4,7 @@
  * npm install express
  * npm install nodemon
  * npx nodemon app.js
- * 
+ *
  * npm install sequelize
  * npm install pg
  * npm install sequelize-cli
@@ -12,20 +12,46 @@
  * npx sequelize-cli init
  */
 
+/**
+ * Sequelize ORM
+ * npm install sequelize
+ * npm install pg
+ * npm install sequelize-cli
+ *  */
+
+/**
+ * Perintah sequelize-cli
+ * npx sequelize-cli
+ * npx sequelize-cli init
+ * npx sequelize-cli db:create (membuat database)
+ */
+
+// Membuat model
+/**
+ * Membuat migration
+ * migrate
+ */
+
+/**
+ * Nama model dibuat singular
+ * npx sequelize-cli model:generate --name todo --attributes task:string, status:boolean
+ * npx sequelize-cli db:migrate
+ */
+
 // Inisialisasi express js
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 // setting agar bisa request yang seharusnya dari frontend bisa diganti ke postman
 // request
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // menggunakan folder routes sebagai routing
-const routes = require('./routes');
+const routes = require("./routes");
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
--- EXPRESS-SEQUELIZE/controllers/TodoController.js
+++ EXPRESS-SEQUELIZE/controllers/TodoController.js
@@ -1,88 +1,98 @@
-const {todo} = require('../models')
+const { todo } = require("../models");
 
 class TodoController {
-  static getTodos (req, res) {
-    todo.findAll()
-      .then(todos => {
+  static getTodos(req, res) {
+    todo
+      .findAll()
+      .then((todos) => {
         res.json(todos);
       })
-      .catch(err => {
+      .catch((err) => {
         res.json(err);
-      })
-  } 
+      });
+  }
 
-  static addTodo (req, res) {
-    const {task, status} = req.body;
+  static addTodo(req, res) {
+    const { task, status } = req.body;
 
-    todo.create({
-      task, status
-    })
-      .then(result => {
-        res.json(result)
+    todo
+      .create({
+        task,
+        status,
       })
-      .catch(err => {
-        res.json(err)
+      .then((result) => {
+        res.json(result);
       })
+      .catch((err) => {
+        res.json(err);
+      });
   }
 
   static findById(req, res) {
     let id = +req.params.id;
-    todo.findByPk(id)
-      .then(result => {
-        if(result !== null) {
+    todo
+      .findByPk(id)
+      .then((result) => {
+        if (result !== null) {
           res.json(result);
         } else {
           res.json({
-            message: "Todo not found!"
-          })
+            message: "Todo not found!",
+          });
         }
       })
-      .catch(err => {
+      .catch((err) => {
         res.json(err);
-      })
+      });
   }
 
   static deleteTodo(req, res) {
     let id = +req.params.id;
 
-    todo.destroy({
-      where: {
-        id
-      }
-    })
-      .then(result => {
-        if(result === 1) {
+    todo
+      .destroy({
+        where: {
+          id,
+        },
+      })
+      .then((result) => {
+        if (result === 1) {
           res.json({
-            message: "Todo has been deleted!"
-          })
+            message: "Todo has been deleted!",
+          });
         } else {
           res.json({
-            message: "Todo failed to delete!"
-          })
+            message: "Todo failed to delete!",
+          });
         }
       })
-      .catch(err => {
+      .catch((err) => {
         res.json(err);
-      })
+      });
   }
 
   static updateTodo(req, res) {
     let id = +req.params.id;
-    const {task, status} = req.body;
+    const { task, status } = req.body;
 
-    todo.update({
-      task, status: Boolean(status)
-    }, {
-      where: {
-        id
-      }
-    })
-      .then(result => {
-        res.json(result)
+    todo
+      .update(
+        {
+          task,
+          status: Boolean(status),
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      )
+      .then((result) => {
+        res.json(result);
       })
-      .catch(err => {
-        res.json(err)
-      })
+      .catch((err) => {
+        res.json(err);
+      });
   }
 }
 
